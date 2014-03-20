b355988add93c0bf095dde11b24e32fba8697e01
Diego Ortin <dieortin@gmail.com>
2014-03-21 00:06:16 +0100

    Changed 'new message' icon and added 'info' icon, started creating bubble structure for conversation view, fixed some bugs

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var conversationSection = document.getElementsByClassName("conversation")[0],
     headerLeftIcon = document.getElementsByTagName("header")[0].children[0],
+    headerRightIcon = document.getElementsByTagName("header")[0].children[2],
   openConversation = function (conversation) {
-    conversationSection.style.left = "2%";
+    conversationSection.style.left = "0%";
     headerLeftIcon.src = "./media/icons/png/arrow-back5_white.png";
+    headerRightIcon.src = "./media/icons/png/info_white.png";
     headerLeftIcon.onclick = backToList;
   },
     backToList = function () {
-      conversationSection.style.left = "99%";
+      conversationSection.style.left = "100%";
       headerLeftIcon.src = "./media/icons/png/more_white.png";
+      headerRightIcon.src = "./media/icons/png/edit_white.png";
       headerLeftIcon.onclick = "alert('still nothing here')";
     }
